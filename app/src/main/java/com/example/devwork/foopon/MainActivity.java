dcccfa1631903dbe5ff8ceaced57b97895038dc4
DevWork <s-RRashid@lwsd.org>
2017-07-26 15:41:56 -0700

    clean up

--- app/src/main/java/com/example/devwork/foopon/MainActivity.java
+++ app/src/main/java/com/example/devwork/foopon/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.devwork.foopon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 public class MainActivity extends Activity {
     private MapView mapView;
-    static String API_KEY = " pk.eyJ1IjoiemFjaGNoZXUiLCJhIjoiY2o1bGtlbzY1MnVueDJ3bnpoZzNpNDltaiJ9.QVTOtZaWnjpXgG0U9nZTeA";
+    static String API_KEY = "pk.eyJ1IjoiemFjaGNoZXUiLCJhIjoiY2o1bGtlbzY1MnVueDJ3bnpoZzNpNDltaiJ9.QVTOtZaWnjpXgG0U9nZTeA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapboxMap.setMyLocationEnabled(true);
+                System.out.println(mapboxMap.getMyLocation().getLatitude());
+                /*
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(mapboxMap.getMyLocation()))
+                        .zoom(13)
+                        .bearing(180)
+                        .tilt(30)
+                        .build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 5000);
+                */
+            }
+        });
     }
 
     @Override
